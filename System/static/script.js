41a1502c0a13c84812102aa7b1b2b180e35e5066
Vaibhav <vk10decvaibhav@gmail.com>
2021-02-11 02:57:10 +0530

    Logic done. Few bug fixes left + Integration with PostgreSQL left

--- System/static/script.js
+++ System/static/script.js
@@ -1,9 +1,19 @@
 var fields = document.querySelector(".Fields");
 var addColumnButton = document.querySelector(".Add-Column-Button");
-var fieldCount = 1;
+var fieldCount;
 var messageCross = document.querySelector("#Message img");
 var message = document.querySelector("#Message");
 var numberOfColumns = document.getElementById("Number-Of-Columns");
+var fieldCrosses = document.querySelectorAll(".Field-Cross");
+var firstTime = true;
+
+function eliminateField() {
+    this.parentNode.remove();
+    numberOfColumns.value--;
+}
+
+for (var i = 0 ; i < fieldCrosses.length ; i++)
+        fieldCrosses[i].addEventListener("click", eliminateField);
 
 messageCross.onclick = function() {
     message.remove();
@@ -11,39 +21,88 @@ messageCross.onclick = function() {
 
 addColumnButton.onclick = function(event) {
     event.preventDefault();
-    numberOfColumns.value++;
-    var fieldHTML = `<div class="Field">
-    <label class="Label">Column Name</label>
-    <input type="text" name="columnName${fieldCount}" placeholder="Enter the column name" class="Input-Field"></input>
-    <label class="Label">Field Type</label>
-    <select class="Field-Type" name="fieldType${fieldCount}">
-        <option value="AutoF">AutoField</option>
-        <option value="BigAutoF">BigAutoField</option>
-        <option value="BigIntegerF">BigIntegerField</option>
-        <option value="BinaryF">BinaryField</option>
-        <option value="BooleanF">BooleanField</option>
-        <option value="CharF">CharField</option>
-        <option value="DateF">DateField</option>
-        <option value="DateTimeF">DateTimeField</option>
-        <option value="DecimalF">DecimalField</option>
-        <option value="IntegerF">IntegerField</option>
-        <option value="DurationF">DurationField</option>
-        <option value="EmailF">EmailField</option>
-        <option value="FileF">FileField</option>
-        <option value="FloatF">FloatField</option>
-        <option value="ImageF">ImageField</option>
-        <option value="NullBooleanF">NullBooleanField</option>
-        <option value="PositiveBigIntegerF">PositiveBigIntegerField</option>
-        <option value="PositiveIntegerF">PositiveIntegerField</option>
-        <option value="SlugF">SlugField</option>
-        <option value="SmallAutoF">SmallAutoField</option>
-        <option value="SmallIntegerF">SmallIntegerField</option>
-        <option value="TextF">TextField</option>
-        <option value="TimeF">TimeField</option>
-        <option value="URLF">URLField</option>
-        <option value="UUIDF">UUIDField</option>
-    </select>
-</div>`;
+
+    if (firstTime) {
+        for (var i = 0 ; i < numberOfColumns.value ; i++) {
+            var fieldHTML = `<div class="Field">
+                <img class="Field-Cross" src="../static/Cross.png" alt="">
+                <label class="Label">Column Name</label>
+                <input type="text" name="columnName${i + 1}" placeholder="Enter the column name" class="Input-Field"></input>
+                <label class="Label">Field Type</label>
+                <select class="Field-Type" name="fieldType${i + 1}">
+                    <option value="AutoF">AutoField</option>
+                    <option value="BigAutoF">BigAutoField</option>
+                    <option value="BigIntegerF">BigIntegerField</option>
+                    <option value="BinaryF">BinaryField</option>
+                    <option value="BooleanF">BooleanField</option>
+                    <option value="CharF">CharField</option>
+                    <option value="DateF">DateField</option>
+                    <option value="DateTimeF">DateTimeField</option>
+                    <option value="DecimalF">DecimalField</option>
+                    <option value="IntegerF">IntegerField</option>
+                    <option value="DurationF">DurationField</option>
+                    <option value="EmailF">EmailField</option>
+                    <option value="FileF">FileField</option>
+                    <option value="FloatF">FloatField</option>
+                    <option value="ImageF">ImageField</option>
+                    <option value="NullBooleanF">NullBooleanField</option>
+                    <option value="PositiveBigIntegerF">PositiveBigIntegerField</option>
+                    <option value="PositiveIntegerF">PositiveIntegerField</option>
+                    <option value="SlugF">SlugField</option>
+                    <option value="SmallAutoF">SmallAutoField</option>
+                    <option value="SmallIntegerF">SmallIntegerField</option>
+                    <option value="TextF">TextField</option>
+                    <option value="TimeF">TimeField</option>
+                    <option value="URLF">URLField</option>
+                    <option value="UUIDF">UUIDField</option>
+                </select>
+            </div>`;
+            fields.insertAdjacentHTML("beforeend", fieldHTML);
+        }
+
+        firstTime = false;
+        fieldCount = numberOfColumns;
+    } else {
+        numberOfColumns.value++;
+        var fieldHTML = `<div class="Field">
+        <img class="Field-Cross" src="../static/Cross.png" alt="">
+        <label class="Label">Column Name</label>
+        <input type="text" name="columnName${fieldCount}" placeholder="Enter the column name" class="Input-Field"></input>
+        <label class="Label">Field Type</label>
+        <select class="Field-Type" name="fieldType${fieldCount}">
+            <option value="AutoF">AutoField</option>
+            <option value="BigAutoF">BigAutoField</option>
+            <option value="BigIntegerF">BigIntegerField</option>
+            <option value="BinaryF">BinaryField</option>
+            <option value="BooleanF">BooleanField</option>
+            <option value="CharF">CharField</option>
+            <option value="DateF">DateField</option>
+            <option value="DateTimeF">DateTimeField</option>
+            <option value="DecimalF">DecimalField</option>
+            <option value="IntegerF">IntegerField</option>
+            <option value="DurationF">DurationField</option>
+            <option value="EmailF">EmailField</option>
+            <option value="FileF">FileField</option>
+            <option value="FloatF">FloatField</option>
+            <option value="ImageF">ImageField</option>
+            <option value="NullBooleanF">NullBooleanField</option>
+            <option value="PositiveBigIntegerF">PositiveBigIntegerField</option>
+            <option value="PositiveIntegerF">PositiveIntegerField</option>
+            <option value="SlugF">SlugField</option>
+            <option value="SmallAutoF">SmallAutoField</option>
+            <option value="SmallIntegerF">SmallIntegerField</option>
+            <option value="TextF">TextField</option>
+            <option value="TimeF">TimeField</option>
+            <option value="URLF">URLField</option>
+            <option value="UUIDF">UUIDField</option>
+        </select>
+    </div>`;
     fieldCount++;
     fields.insertAdjacentHTML("beforeend", fieldHTML);
+    }
+
+    fieldCrosses = document.querySelectorAll(".Field-Cross");
+
+    for (var i = 0 ; i < fieldCrosses.length ; i++)
+        fieldCrosses[i].addEventListener("click", eliminateField);
 };
--- System/views.py
+++ System/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.db import models
 from django.contrib import admin
+from django.core.management import call_command
+from django.db import connection
+from django.urls import clear_url_caches
+from importlib import import_module, reload
+from django.urls import reverse
+from django.conf import settings
 
-def createModel(schemaName, tableName, numberOfColumns, fields):
+def createModel(schemaName, tableName, fields):
     class Meta:
         pass
 
-    setattr(Meta, "app_label", "System")
-
-    fields = {'__module__': "", 'Meta': Meta}
-    model =  type(tableName, (models.Model, ), fields)
-    admin.site.register(model)
-    print(model)
+    setattr(Meta, "app_label", "System") # Setting up the app label
+    fields['__module__'] = "System.models"
+    fields['Meta'] =  Meta
+    model = type(tableName, (models.Model, ), fields) # Creating the model
+    call_command('makemigrations') # Making migrations
+    call_command('migrate') # Migrating
+    admin.site.register(model) # Registering model
+    reload(import_module(settings.ROOT_URLCONF)) # Updating url
+    clear_url_caches() # Clearing caches of urls since urls got updated
 
     return model
 
@@ -26,79 +35,46 @@ def index(request):
         for i in range(numberOfColumns):
             fields[request.POST[f"columnName{i + 1}"]] = fieldsDataTypes[request.POST[f"fieldType{i + 1}"]]
 
-        print(schemaName)
-        print(tableName)
-        print(numberOfColumns)
-        createModel(schemaName, tableName, numberOfColumns, fields)
-
-        for x in fields:
-            print(f"{x} --> {fields[x]}")
-
-        return render(request, "index.html", context = {
-            "message": "Model created successfully!"
-        })
-    return render(request, "index.html", context = {
-        "message": "Some error occurred! Try again."
-    })
+        model = createModel(schemaName, tableName, fields)
+        
+        if model:
+            return render(request, "index.html", context = {
+                "message": "Model created successfully!"
+            })
+        else:
+            return render(request, "index.html", context = {
+                "message": "Some error occurred! Try again."
+            })
+            
+    return render(request, "index.html", context = {})
 
 fieldsDataTypes = {
-    "AutoF": "models.AutoField()",
-    "BigAutoF": "models.BigAutoField()",
-    "BigIntegerF": "models.BigIntegerField()",
-    "BinaryF": "models.BinaryField()",
-    "BooleanF": "models.BooleanField()",
-    "CharF": "models.CharField(max_length = 200)",
-    "DateF": "models.DateField()",
-    "DateTimeF": "models.DateTimeField()",
-    "DecimalF": "models.DecimalField()",
-    "IntegerF": "models.IntegerField()",
-    "DurationF": "models.DurationField()",
-    "EmailF": "models.EmailField(max_length = 200)",
-    "FileF": "models.FileField(upload_to = Uploads/Files/)",
-    "FloatF": "models.FloatField()",
-    "ImageF": "models.ImageField(upload_to = Uploads/Images/)",
-    "NullBooleanF": "models.NullBooleanField()",
-    "PositiveBigIntegerF": "models.PositiveBigIntegerField()",
-    "PositiveIntegerF": "models.PositiveIntegerField()",
-    "SlugF": "models.(max_length = 200)",
-    "SmallAutoF": "models.SmallAutoField()",
-    "SmallIntegerF": "models.SmallIntegerField()",
-    "TextF": "models.TextField()",
-    "TimeF": "models.TimeField()",
-    "URLF": "models.URLField(max_length=200)",
-    "UUIDF": "models.UUIDField()",
-    "ForeignK": "models.ForeignKey(on_delete = models.CASCADE)",
-    "ManyToManyF": "models.ManyToManyField()",
-    "OneToOneF": "models.OneToOneField(on_delete = models.CASCADE)",
+    "AutoF": models.AutoField(),
+    "BigAutoF": models.BigAutoField(),
+    "BigIntegerF": models.BigIntegerField(),
+    "BinaryF": models.BinaryField(),
+    "BooleanF": models.BooleanField(),
+    "CharF": models.CharField(max_length = 200),
+    "DateF": models.DateField(),
+    "DateTimeF": models.DateTimeField(),
+    "DecimalF": models.DecimalField(),
+    "IntegerF": models.IntegerField(),
+    "DurationF": models.DurationField(),
+    "EmailF": models.EmailField(max_length = 200),
+    "FileF": models.FileField(upload_to = "Uploads/Files/"),
+    "FloatF": models.FloatField(),
+    "ImageF": models.ImageField(upload_to = "Uploads/Images/"),
+    "NullBooleanF": models.NullBooleanField(),
+    "PositiveBigIntegerF": models.PositiveBigIntegerField(),
+    "PositiveIntegerF": models.PositiveIntegerField(),
+    "SlugF": models.SlugField(max_length = 200),
+    "SmallAutoF": models.SmallAutoField(),
+    "SmallIntegerF": models.SmallIntegerField(),
+    "TextF": models.TextField(),
+    "TimeF": models.TimeField(),
+    "URLF": models.URLField(max_length=200),
+    "UUIDF": models.UUIDField(),
+    # "ForeignK": models.ForeignKey({model}, on_delete = models.CASCADE),
+    # "ManyToManyF": models.ManyToManyField({model}),
+    # "OneToOneF": models.OneToOneField({model}, on_delete = models.CASCADE),
 }
-
-
-    
-    # <option value="AutoF">AutoField</option>
-    # <option value="BigAutoF">BigAutoField</option>
-    # <option value="BigIntegerF">BigIntegerField</option>
-    # <option value="BinaryF">BinaryField</option>
-    # <option value="BooleanF">BooleanField</option>
-    # <option value="CharF">CharField</option>
-    # <option value="DateF">DateField</option>
-    # <option value="DateTimeF">DateTimeField</option>
-    # <option value="DecimalF">DecimalField</option>
-    # <option value="IntegerF">IntegerField</option>
-    # <option value="DurationF">DurationField</option>
-    # <option value="EmailF">EmailField</option>
-    # <option value="FileF">FileField</option>
-    # <option value="FloatF">FloatField</option>
-    # <option value="ImageF">ImageField</option>
-    # <option value="NullBooleanF">NullBooleanField</option>
-    # <option value="PositiveBigIntegerF">PositiveBigIntegerField</option>
-    # <option value="PositiveIntegerF">PositiveIntegerField</option>
-    # <option value="SlugF">SlugField</option>
-    # <option value="SmallAutoF">SmallAutoField</option>
-    # <option value="SmallIntegerF">SmallIntegerField</option>
-    # <option value="TextF">TextField</option>
-    # <option value="TimeF">TimeField</option>
-    # <option value="URLF">URLField</option>
-    # <option value="UUIDF">UUIDField</option>
-    # <option value="ForeignK">ForeignKey</option>
-    # <option value="ManyToManyF">ManyToManyField</option>
-    # <option value="OneToOneF">OneToOneField</option>
